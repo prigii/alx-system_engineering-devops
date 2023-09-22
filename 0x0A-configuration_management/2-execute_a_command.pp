f3e2ee5b0cf00e1760b6df75c8219d38778f6b5f
Peter Rigii <theonlyrigii@gmail.com>
2023-09-22 23:24:02 +0300

    corrected

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # Kills a process name killmenow
 
 exec { 'pkill killmenow':
-  path     => '/usr/bin/pkill killmenow',
+  path     => '/usr/bin/killmenow',
   command  => 'pkill killmenow',
   provider => shell,
   returns  => [0, 1],
