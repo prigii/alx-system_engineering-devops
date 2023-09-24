3d64d7c4d4a1848c12024f5cb8c6475cb5fd55f4
Peter Rigii <theonlyrigii@gmail.com>
2023-09-24 12:49:42 +0300

    client config w/ puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,7 @@
 # puppet script that sets up ssh config file to allow connectivity without password
-include stdlib
-
-file_line { 'Turn off passwd auth':
-  path => '/etc/ssh/sshd_config',  # Path to the SSH server configuration file
-  line => 'PasswordAuthentication no',
-}
-
-file_line { 'Declare identity file':
-  path => '/etc/ssh/ssh_config',  # Path to the SSH client configuration file
-  line => 'IdentityFile ~/.ssh/school',
+# Changes SSH config file
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
 }
