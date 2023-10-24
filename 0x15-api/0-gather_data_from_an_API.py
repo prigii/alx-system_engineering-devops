113ceb39c1e7007ca8156bdc815d1e894cf2f6f1
Peter Rigii <theonlyrigii@gmail.com>
2023-10-24 10:17:26 +0300

    edited

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""Makes a get request to json placeholder"""
 import requests
-import sys
+from sys import argv
+
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    """main api method"""
+    url = 'https://jsonplaceholder.typicode.com/'
+    res = requests.get(url + "users/{}".format(argv[1]))
+    res_t = requests.get(url + "todos", params={"userId": argv[1]})
+    emp = res.json()
+    todo = res_t.json()
+    c = [item for item in todo if item.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    for c in completed:
-        print("\t {}".format(c))
+        emp.get("name"), len(c), len(todo)))
+    for item in c:
+        print('\t {}'.format(item.get("title")))
