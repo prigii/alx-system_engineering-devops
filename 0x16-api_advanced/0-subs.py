3275a050e1503367e6c5b65e361afdb957e6ebb8
Peter Rigii <theonlyrigii@gmail.com>
2023-11-07 15:44:38 +0300

    reddit subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,30 @@
+#!/bin/usr/python3
+""" Imports number of subs from a reddit page """
+import requests
+
+def number_of_subscribers(subreddit):
+    # Define the Reddit API URL for the given subreddit
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    # Set a custom User-Agent to avoid issues
+    headers = {'User-Agent': 'MyBot/1.0'}
+
+    try:
+        # Send a GET request to the API
+        response = requests.get(url, headers=headers)
+
+        # Check if the response is successful
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+
+            # Extract the number of subscribers
+            subscribers = data['data']['subscribers']
+
+            return subscribers
+        else:
+            # If the subreddit is invalid or other error occurred, return 0
+            return 0
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,2 @@
+0x16. API advanced
+0. How many subs?
